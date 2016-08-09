41ab4093d72b6749a2de160a9a2ce1434917d67d
Will Davis <Hipotane@gmail.com>
2016-08-09 18:35:12 +0100

    added comments

--- solution-1/script.js
+++ solution-1/script.js
@@ -1,11 +1,20 @@
+/************************************************
+In this Problem I had to find a way to create a sum out of all the multiples
+of 3 & 5 bellow 1000. I chose to do this by using iteration with a 'for' loop
+then using a conditional statement to determine whether the value was a
+multiple. Finally, I added them together in the variable "sum".
+************************************************/
+
 var sum = 0;
 var html;
 
+//inserts a meesage in to the HTML file.
 function print(message) {
   var div = document.getElementById('output');
   div.innerHTML  = message;
 }
 
+//Returns a sum of all the multiples of 3 & 5 up to 1000.
 for (var i= 1;i<1000; i++ ){
   if (i%3 === 0 || i%5 === 0){
   sum = sum + i;
--- solution-3/script.js
+++ solution-3/script.js
@@ -1,13 +1,25 @@
+/************************************************
+In this problem I had to log the prime factors of a large number and return
+the largest factor. To do this I decided to use a loop to iterate through
+numbers one by one and check if they were divisible by the large number, then
+storing the divided value. I added a prompt because I wanted to practice taking
+and manipulating user input. The print method is to practice manipulating the
+HTML DOM.
+************************************************/
+
+//Asks user to enter a number they would like to factor.
 var value = prompt('Enter a number that you would like to find the prime factors of');
 var primeFac = 0;
 var i = 2;
 
+//inserts a meesage in to the HTML file.
 function print(message) {
   var div = document.getElementById('output');
   div.innerHTML  = message;
 }
 
-
+/*Looks at all the prime numbers of the users number and stores the last, being
+the largest.*/
 while (i < value) {
   if (value%i === 0) {
     value /= i;
